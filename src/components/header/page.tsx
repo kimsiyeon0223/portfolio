4a2f15676b09de3cf1396b1e04002da7a6808a1c
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-13 17:42:51 +0900

    feat : 해더 컴포넌트 퍼블리싱

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,7 +1,26 @@
-export const Header = () => {
+import Link from "next/link";
+import * as S from "./style.css";
+import classNames from "classnames";
+
+interface ttype {
+  color?: boolean;
+}
+
+export const Header = ({ color }: ttype) => {
   return (
-    <div>
-      <div>asdfasdf</div>
+    <div className={classNames(S.Layout, { [S.LayoutColor]: color })}>
+      <h1 className={S.Title}>PORTIFY</h1>
+      <div className={S.Bars}>
+        <Link href="/ai">
+          <span className={S.AI}>AI 분석</span>
+        </Link>
+        <Link href="/write">
+          <span className={S.Write}>포트폴리오 작성</span>
+        </Link>
+        <Link href="/my-portfolio">
+          <span className={S.My}>내 포트폴리오</span>
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ src/components/header/style.css.ts
@@ -0,0 +1,38 @@
+import { style } from "@vanilla-extract/css";
+
+export const Layout = style({
+  display: "flex",
+  width: "100%",
+  padding: "20px 100px",
+  color: "white",
+});
+
+export const LayoutColor = style({
+  color: "#274696",
+  backgroundColor: "white",
+});
+
+export const Title = style({
+  fontFamily: "Rubik Scribble",
+});
+
+export const Bars = style({
+  fontFamily: "Pretendard-Regular",
+  marginLeft: "auto",
+  display: "flex",
+  alignItems: "center",
+  gap: "50px",
+  fontSize: "17px",
+});
+
+export const AI = style({
+  cursor: "pointer",
+});
+
+export const Write = style({
+  cursor: "pointer",
+});
+
+export const My = style({
+  cursor: "pointer",
+});
