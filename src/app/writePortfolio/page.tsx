90e736110128c3ccd0bd902ae801f149d2c3a9ee
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-14 20:01:25 +0900

    feat : 작성한 포트폴리오 로컬 스토리지에 저장

--- src/app/writePortfolio/page.tsx
+++ src/app/writePortfolio/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { Header } from "@/components/header/page";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as S from "./style.css";
 import MDEditor from "@uiw/react-md-editor";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function WritePortfolio() {
   const [content, setContent] = useState("");
@@ -12,12 +13,39 @@ export default function WritePortfolio() {
   const [inputTag, setInputTag] = useState("");
   const [tags, setTags] = useState<Array<string>>([]);
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedTitle = localStorage.getItem("title");
+    const storedContent = localStorage.getItem("content");
+    const storedTags = localStorage.getItem("tags");
+
+    if (storedTitle) setTitle(storedTitle);
+    if (storedContent) setContent(storedContent);
+    if (storedTags) setTags(storedTags.split(","));
+  }, []);
+
   const handleOnKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter" && inputTag !== "") {
       setTags([...tags, inputTag]);
       setInputTag("");
     }
   };
+
+  const handleSubmit = () => {
+    localStorage.setItem("title", title);
+    localStorage.setItem("content", content);
+    localStorage.setItem("tags", tags.join(","));
+
+    router.push(
+      `/showPortfolio?contents=${encodeURIComponent(
+        content,
+      )}&titles=${encodeURIComponent(title)}&tags=${encodeURIComponent(
+        tags.join(","),
+      )}`,
+    );
+  };
+
   return (
     <main className={S.Layout}>
       <Header color />
@@ -62,7 +90,9 @@ export default function WritePortfolio() {
         <Link href="/">
           <span className={S.GetOut}>{"<-"} 나가기</span>
         </Link>
-        <button className={S.btn}>올리기</button>
+        <button onClick={handleSubmit} className={S.btn}>
+          올리기
+        </button>
       </footer>
     </main>
   );
